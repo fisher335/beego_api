0d038e759b59446cb39071c6b1c74ae62313a299
Terry <fengshaomin@buaa.edu.cn>
2023-03-08 23:38:26 +0800

    结构体

--- controllers/user.go
+++ controllers/user.go
@@ -9,19 +9,10 @@ import (
 	beego "github.com/beego/beego/v2/server/web"
 )
 
-// UserController
-// Operations about Users
 type UserController struct {
 	beego.Controller
 }
 
-// Post
-// @Title CreateUser
-// @Description create users
-// @Param	body		body 	models.User	true		"body for user content"
-// @Success 200 {int} models.User.Id
-// @Failure 403 body is empty
-// @router / [post]
 func (u *UserController) Post() {
 	var user models.User
 	json.Unmarshal(u.Ctx.Input.RequestBody, &user)
@@ -30,10 +21,6 @@ func (u *UserController) Post() {
 	u.ServeJSON()
 }
 
-// GetAll @Title GetAll
-// @Description get all Users
-// @Success 200 {object} models.User
-// @router / [get]
 func (u *UserController) GetAll() {
 	users := models.GetAllUsers()
 	u.Data["json"] = users
@@ -95,10 +82,6 @@ func (u *UserController) Login() {
 	u.ServeJSON()
 }
 
-// @Title logout
-// @Description Logs out current logged in user session
-// @Success 200 {string} logout success
-// @router /logout [get]
 func (u *UserController) Logout() {
 	u.Data["json"] = "logout success"
 	u.ServeJSON()
